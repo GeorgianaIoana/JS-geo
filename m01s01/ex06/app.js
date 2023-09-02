488963d53dfe083cf5f99480985c5691da49a363
GeorgianaIoana <victornedelcu90@gmail.com>
2023-09-02 20:30:46 +0300

    tema1JS

--- m01s01/ex06/app.js
+++ m01s01/ex06/app.js
@@ -1,18 +1,15 @@
-var inputLength = document.getElementById('length');
-var inputWidth = document.getElementById('width');
+var inputRadius = document.getElementById('radius');
 var elementResult = document.getElementById('result');
 var form = document.querySelector('form');
 
 form.addEventListener(
   'submit',
+
   function (event) {
     event.preventDefault();
 
-    var length = inputLength.value || 0;
-    var width = inputWidth.value || 0;
-    var result = 0;
-
-    result = length * width;
+    var radius = inputRadius.value || 0;
+    var result = 4 * Math.PI * Math.pow(radius, 2);
 
     elementResult.innerText = result;
   },
